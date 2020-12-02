b17e891799a18fea889656d4551151180b733606
Bruno <bruno@localhost.localdomain>
2020-12-02 18:01:32 -0300

    Filter adicionado

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,7 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
 
     $scope.fotos = [];
-    
+    $scope.filtro = '';
 
     $http.get('v1/fotos')
     .success(function(retorno){
