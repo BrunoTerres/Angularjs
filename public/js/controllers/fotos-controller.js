f4b164dd2e8d214c2e5058186af649b86967fae5
Bruno <bruno@localhost.localdomain>
2020-12-02 13:59:33 -0300

    ng-repeat fotos

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,18 @@
 angular.module('alurapic').controller('FotosController', function($scope){
 
-    $scope.foto = {
-        titulo: "Foto Lua",
-        url: "https://imagens.canaltech.com.br/236607.471195-Lua.jpg"
-    };
+    $scope.fotos = [
+        {
+            titulo: "Lua",
+            url: "https://imagens.canaltech.com.br/236607.471195-Lua.jpg"
+        },
+        {
+            titulo: "Jureg Comuna",
+            url: "https://uploads.spiritfanfiction.com/fanfics/historias/201610/as-aventuras-do-adm-comunista-e-o-burro-falante-do-jureg-6647284-041020161940.png"
+        },
+        {
+            titulo: "Sol",
+            url: 'https://programadoresbrasil.com.br/wp-content/uploads/2020/07/nasa-registra-foto-do-sol-mais-proxima-ja-feita-na-historia.jpg'
+        }
+    ];
 
 });
