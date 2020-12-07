98c4de233e853bfaa3b7dff773c9918a2ad4d78d
Bruno <bruno@localhost.localdomain>
2020-12-07 10:43:44 -0300

    Caminhos corrigidos/ Criação partial e index.html dinamico

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -14,4 +14,19 @@ angular.module('minhasDiretivas', [])
         ddo.templateUrl = 'js/directives/meu-painel.html';
 
 		return ddo;
-	});
+	})
+    .directive('minhaFoto', function() {
+
+        var ddo = {};
+
+        ddo.restrict = "AE";
+
+        ddo.scope = {
+            titulo: '@',
+            url: '@'
+        };
+
+        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
+        
+        return ddo;
+    });
