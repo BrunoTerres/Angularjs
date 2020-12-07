0368f5c063dd3119b328b705d61aa9f91ab2f9f6
Bruno <bruno@localhost.localdomain>
2020-12-07 14:23:23 -0300

    tratamento saida de erros para insercao de fotos

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,15 +1,21 @@
 angular.module('alurapic').controller('FotoController', function($scope, $http) {
     
     $scope.foto = {};
+    $scope.mensagem = '';
     $scope.exibe = false;
 
-    $scope.submeter = function() {
-        $http.post("v1/fotos", $scope.foto)
-        .success(function() {
 
-        })
-        .error(function(erro) {
-            console.log(erro)
-        })
+    $scope.submeter = function() {
+        if ($scope.formulario.$valid) {
+            $http.post("v1/fotos", $scope.foto)
+            .success(function() {
+                $scope.foto = {};
+                $scope.mensagem = "Foto incluída com sucesso";
+            })
+            .error(function(erro) {
+                $scope.mensagem = "Não foi possível incluir a foto"
+                console.log(erro)
+            })
+        }
     }
 })
